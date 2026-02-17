261bfb5d3f9e4cfaed8da59fe761df2318e583ed
sharmavimal7467 <sharmavimal7467@gmail.com>
2026-02-17 10:46:47 +0530

    typescript fix in type file

--- src/data/initialData.ts
+++ src/data/initialData.ts
@@ -1,37 +1,75 @@
-import type { TreeNodeModel } from "../components/TreeView/tree.types";
+// import type { TreeNodeModel } from "../components/TreeView/tree.types";
 
+// export const initialData: TreeNodeModel[] = [
+//   {
+//     id: "1",
+//     name: "A",
+//     parentId: null,
+//     hasChildren: true,
+//     isExpanded: true,
+//     children: [
+//       {
+//         id: "2",
+//         name: "B",
+//         parentId: "1",
+//         hasChildren: true,
+//         isExpanded: true,
+//         children: [
+//           {
+//             id: "3",
+//             name: "C",
+//             parentId: "2",
+//             hasChildren: true,
+//             isExpanded: true,
+//             children: [
+//               {
+//                 id: "4",
+//                 name: "D",
+//                 parentId: "3",
+//                 hasChildren: false,
+//               }
+//             ]
+//           }
+//         ]
+//       }
+//     ]
+//   }
+// ];
+
+import type { TreeNodeModel } from "../components/TreeView/tree.types";
 export const initialData: TreeNodeModel[] = [
   {
     id: "1",
     name: "A",
-    parentId: null,
     hasChildren: true,
     isExpanded: true,
+    isLoading: false,
     children: [
       {
         id: "2",
         name: "B",
-        parentId: "1",
         hasChildren: true,
         isExpanded: true,
+        isLoading: false,
         children: [
           {
             id: "3",
             name: "C",
-            parentId: "2",
             hasChildren: true,
             isExpanded: true,
+            isLoading: false,
             children: [
               {
                 id: "4",
                 name: "D",
-                parentId: "3",
                 hasChildren: false,
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
+                isExpanded: false,
+                isLoading: false,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ];
